1fd032d2acc5be6afe6fdd2d8648b9b7c5cc5c8d
daniel85111 <danielba@ua.pt>
2022-05-16 18:25:03 +0100

    update

--- build_nrf52840dk_nrf52840/zephyr/CMakeFiles/zephyr.dir/lib/os/crc32c_sw.c.obj.d
+++
@@ -1,9 +0,0 @@
-zephyr/CMakeFiles/zephyr.dir/lib/os/crc32c_sw.c.obj: \
- C:/Users/marco/ncs/v1.9.1/zephyr/lib/os/crc32c_sw.c \
- D:/2021-2022/SETR/Pratica/A3_VendingMachine/build_nrf52840dk_nrf52840/zephyr/include/generated/autoconf.h \
- C:/Users/marco/ncs/v1.9.1/zephyr/include/toolchain/zephyr_stdint.h \
- C:/Users/marco/ncs/v1.9.1/zephyr/include/sys/crc.h \
- C:/Users/marco/ncs/v1.9.1/zephyr/include/zephyr/types.h \
- c:\users\marco\ncs\v1.9.1\toolchain\opt\lib\gcc\arm-none-eabi\9.2.1\include\stddef.h \
- C:/Users/marco/ncs/v1.9.1/zephyr/lib/libc/minimal/include/stdint.h \
- C:/Users/marco/ncs/v1.9.1/zephyr/lib/libc/minimal/include/stdbool.h
